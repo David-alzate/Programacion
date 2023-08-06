9cfbbfa7d8a0a4dcad958cb78bcc6b22512d9544
David-alzate <alzatedavid0126@gmail.com>
2023-08-05 23:11:17 -0500

    Cambio de ejercicio

--- python/Mateo/Actividad_2/2.py
+++ python/Mateo/Actividad_2/2.py
@@ -1,7 +1,11 @@
-# Algoritmo para saludar
-for  i in range(5):
-    nombre  = str(input("Ingrese su nombre "))
-    print ("Hola ", nombre)
+nombres = []
 
 
+for i in range(5):
+        nombre = (str(input("Ingrese su nombre")))
+        nombres.append(nombre)
+        
+for i in range(5):
+        print("Hola ",nombres[i])
+    
     
