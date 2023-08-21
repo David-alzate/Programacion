c987a9fba01b81dedfe9bef5d0461fde12fdb6c7
David-alzate <alzatedavid0126@gmail.com>
2023-08-21 16:46:12 -0500

    Poo python

---
+++ python/POO/clases y objetos.py
@@ -0,0 +1,13 @@
+class celular:
+    def __init__(self, marca, modelo, camara):
+        self.marca = marca
+        self.modelo = modelo
+        self.camara = camara
+    
+
+
+celular1 = celular("samsung", "s23", "48MX")
+ 
+print(celular1.marca)
+
+
