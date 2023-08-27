5ec196a055a3a4c1d32c99a8c09fcf69355ee9d2
David-alzate <alzatedavid0126@gmail.com>
2023-08-27 17:39:55 -0500

    librerias c++

---
+++ C++/Librerias/funciones.h
@@ -0,0 +1,17 @@
+float suma (int x, int y ){
+    return x + y;
+}
+
+float resta (int x, int y ){
+    return x - y;
+}
+
+float multiplicacion (int x, int y ){
+    return x * y;
+}
+
+
+float division (int x, int y ){
+    return x / y;
+}
+
---
+++ C++/Librerias/operaciones.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "funciones.h"
+
+
+using namespace std;
+
+int main() {
+
+    cout<< suma (1,3) <<endl;
+    cout<< resta(5,2) <<endl;
+    cout<< multiplicacion(10,3) <<endl;
+    cout<< division(100,2) <<endl;
+    
+    return 0;
+}
+
+
+
