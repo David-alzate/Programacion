cf05413727978f93eb77d574710c8ff2378cf180
David-alzate <alzatedavid0126@gmail.com>
2023-09-26 16:33:47 -0500

    ejemplo poo c++

---
+++ C++/Poo/ejemplo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class carro{
+    double aceleracion;
+    double velocidad;
+
+public:
+     void arrancar(){
+        // instrucciones para arrancar 
+     }
+     void frenar(){
+        printf("Frenar");
+     }
+
+};
+
+int main() {
+      
+    return 0;
+}
